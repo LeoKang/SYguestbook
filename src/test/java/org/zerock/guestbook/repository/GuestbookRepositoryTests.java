8217065cb7f7ac2059b3779c7a988ad7c6f6f625
SYU <xeungjin@gmail.com>
2025-03-14 16:12:46 +0900

    23 page, 단일 항목 검색 testQuery1()

--- src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
+++ src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
@@ -1,9 +1,16 @@
 package org.zerock.guestbook.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.guestbook.entity.Guestbook;
+import org.zerock.guestbook.entity.QGuestbook;
 
 import java.util.Optional;
 import java.util.stream.IntStream;
@@ -16,7 +23,7 @@ public class GuestbookRepositoryTests {
 
     @Test
     public void insertDummies() {
-        IntStream.rangeClosed(1, 300).forEach(i->{
+        IntStream.rangeClosed(1, 300).forEach(i -> {
             Guestbook guestbook = Guestbook.builder()
                     .title("Title...." + i)
                     .content("Content..." + i)
@@ -31,7 +38,7 @@ public class GuestbookRepositoryTests {
     public void updateTest() {
         Optional<Guestbook> result = guestbookRepository.findById(300L);
 
-        if(result.isPresent()) {
+        if (result.isPresent()) {
             Guestbook guestbook = result.get();
 
             guestbook.changeTitle("Changed Title.....");
@@ -40,4 +47,20 @@ public class GuestbookRepositoryTests {
             guestbookRepository.save(guestbook);
         }
     }
+
+    @Test
+    public void testQuery1() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("gno").descending());
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+        String keyword = "23";
+        BooleanBuilder builder = new BooleanBuilder();
+        BooleanExpression expression = qGuestbook.title.contains(keyword);
+        builder.and(expression);
+        Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
+
+        result.stream().forEach(guestbook -> {
+            System.out.println(guestbook);
+        });
+    }
+
 }
